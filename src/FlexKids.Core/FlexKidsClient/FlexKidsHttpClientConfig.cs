20384b999a17e71af7538e0f010424ff0ec1335c
Coen van den Munckhof <c.munckhof@gmail.com>
2021-01-21 20:56:21 +0100

    add some comments

--- src/FlexKids.Core/FlexKidsClient/FlexKidsHttpClientConfig.cs
+++ src/FlexKids.Core/FlexKidsClient/FlexKidsHttpClientConfig.cs
@@ -1,5 +1,8 @@
 namespace FlexKids.Core.FlexKidsClient
 {
+    /// <summary>
+    /// Configuration for <see cref="HttpFlexKidsClient"/>.
+    /// </summary>
     public class FlexKidsHttpClientConfig
     {
         public FlexKidsHttpClientConfig(string hostUrl, string username, string password)
@@ -9,10 +12,19 @@ namespace FlexKids.Core.FlexKidsClient
             Password = password;
         }
 
+        /// <summary>
+        /// Gets HostUrl of FlexKids.
+        /// </summary>
         public string HostUrl { get; }
 
+        /// <summary>
+        /// Gets username for logging in to FlexKids.
+        /// </summary>
         public string Username { get; }
 
+        /// <summary>
+        /// Gets password for logging in to FlexKids.
+        /// </summary>
         public string Password { get; }
     }
 }
