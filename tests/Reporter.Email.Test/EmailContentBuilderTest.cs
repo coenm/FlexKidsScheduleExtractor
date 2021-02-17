3dbfe4ea0756143fb8481e945f4e038e82711ba7
Coen van den Munckhof <c.munckhof@gmail.com>
2021-02-17 17:20:26 +0100

    Rewriting test using Verify

--- tests/Reporter.Email.Test/EmailContentBuilderTest.cs
+++ tests/Reporter.Email.Test/EmailContentBuilderTest.cs
@@ -1,12 +1,15 @@
 namespace Reporter.Email.Test
 {
     using System;
+    using System.Threading.Tasks;
     using FlexKids.Core.Repository.Model;
     using FlexKids.Core.Scheduler.Model;
     using FluentAssertions;
+    using VerifyXunit;
     using Xunit;
     using Sut = Reporter.Email.EmailContentBuilder;
 
+    [UsesVerify]
     public class EmailContentBuilderTest
     {
         private readonly WeekSchedule _weekSchedule = new WeekSchedule()
@@ -56,7 +59,7 @@ namespace Reporter.Email.Test
         public void ScheduleToPlainTextStringWithEmptyListReturnsEmptyStringTest()
         {
             // arrange
-            var scheduleDiff = new ScheduleDiff[0];
+            ScheduleDiff[] scheduleDiff = Array.Empty<ScheduleDiff>();
 
             // act
             var result = Sut.ScheduleToPlainTextString(scheduleDiff);
@@ -66,7 +69,7 @@ namespace Reporter.Email.Test
         }
 
         [Fact]
-        public void ScheduleToPlainTextStringWithThreeItemsInListReturnsFormattedStringTest()
+        public async Task ScheduleToPlainTextStringWithThreeItemsInListReturnsFormattedStringTest()
         {
             // arrange
             var scheduleDiff = new ScheduleDiff[]
@@ -92,18 +95,14 @@ namespace Reporter.Email.Test
             var result = Sut.ScheduleToPlainTextString(scheduleDiff);
 
             // assert
-            var expected = string.Empty;
-            expected += "+ 08-04 08:05-17:05 Jacob" + Environment.NewLine;
-            expected += "- 08-01 10:05-12:05 New York" + Environment.NewLine;
-            expected += "= 08-04 08:30-22:00 Madrid" + Environment.NewLine;
-            _ = result.Should().Be(expected);
+            await Verifier.Verify(result);
         }
 
         [Fact]
         public void ScheduleToHtmlStringWithEmptyListReturnsEmptyStringTest()
         {
             // arrange
-            var scheduleDiff = new ScheduleDiff[] { };
+            ScheduleDiff[] scheduleDiff = Array.Empty<ScheduleDiff>();
 
             // act
             var result = Sut.ScheduleToHtmlString(scheduleDiff);
@@ -113,7 +112,7 @@ namespace Reporter.Email.Test
         }
 
         [Fact]
-        public void ScheduleToHtmlStringWithThreeItemsInListReturnsFormattedStringTest()
+        public async Task ScheduleToHtmlStringWithThreeItemsInListReturnsFormattedStringTest()
         {
             // arrange
             var scheduleDiff = new ScheduleDiff[]
@@ -139,45 +138,7 @@ namespace Reporter.Email.Test
             var result = Sut.ScheduleToHtmlString(scheduleDiff);
 
             // assert
-            var expected = @"
-<p>Hier is het rooster voor week 23:</p>
-<table style='border: 1px solid black; border-collapse:collapse;'>
-<tr style='text-align:left; padding:0px 5px; border: 1px solid black;'>
-<td style='text-align:center; padding:0px 5px; border: 1px solid black;'></td>
-<td colspan=2 style='text-align:left; padding:0px 5px; border: 1px solid black;'><b>Dag</b></td>
-<td colspan=3 style='text-align:left; padding:0px 5px; border: 1px solid black;'><b>Tijd</b></td>
-<td style='text-align:left; padding:0px 5px; border: 1px solid black;'><b>Locatie</b></td>
-</tr>
-<tr style='text-align:left; padding:0px 5px; border: 1px solid black;'>
-<td style='text-align:center; padding:0px 5px; border: 1px solid black;'>+</td>
-<td style='text-align:left; padding:0px 5px; border: 1px solid black; border-right:hidden;'>zo</td>
-<td style='text-align:left; padding:0px 5px; border: 1px solid black;'>08-04</td>
-<td style='text-align:left; padding:0px 5px; border: 1px solid black; text-align: right; padding-right:0px;'>08:05</td>
-<td style='text-align:center; padding:0px 5px; border: 1px solid black; border-left: hidden; border-right: hidden;'>-</td>
-<td style='text-align:left; padding:0px 5px; border: 1px solid black; padding-left:0px;'>17:05</td>
-<td style='text-align:left; padding:0px 5px; border: 1px solid black;'>Jacob</td>
-</tr>
-<tr style='text-align:left; padding:0px 5px; border: 1px solid black;'>
-<td style='text-align:center; padding:0px 5px; border: 1px solid black;'>-</td>
-<td style='text-align:left; padding:0px 5px; border: 1px solid black;text-decoration: line-through; border-right:hidden;'>zo</td>
-<td style='text-align:left; padding:0px 5px; border: 1px solid black;text-decoration: line-through;'>08-01</td>
-<td style='text-align:left; padding:0px 5px; border: 1px solid black;text-decoration: line-through; text-align: right; padding-right:0px;'>10:05</td>
-<td style='text-align:center; padding:0px 5px; border: 1px solid black; border-left: hidden; border-right: hidden;'>-</td>
-<td style='text-align:left; padding:0px 5px; border: 1px solid black;text-decoration: line-through; padding-left:0px;'>12:05</td>
-<td style='text-align:left; padding:0px 5px; border: 1px solid black;text-decoration: line-through;'>New York</td>
-</tr>
-<tr style='text-align:left; padding:0px 5px; border: 1px solid black;'>
-<td style='text-align:center; padding:0px 5px; border: 1px solid black;'>=</td>
-<td style='text-align:left; padding:0px 5px; border: 1px solid black; border-right:hidden;'>zo</td>
-<td style='text-align:left; padding:0px 5px; border: 1px solid black;'>08-04</td>
-<td style='text-align:left; padding:0px 5px; border: 1px solid black; text-align: right; padding-right:0px;'>08:30</td>
-<td style='text-align:center; padding:0px 5px; border: 1px solid black; border-left: hidden; border-right: hidden;'>-</td>
-<td style='text-align:left; padding:0px 5px; border: 1px solid black; padding-left:0px;'>22:00</td>
-<td style='text-align:left; padding:0px 5px; border: 1px solid black;'>Madrid</td>
-</tr>
-</table>
-</p>";
-            _ = result.Trim().Should().Be(expected.Trim());
+            await Verifier.Verify(result);
         }
     }
 }
